15fd4844fa80148d8f49a9e00f6934e71b497d9c
osamanazar47 <osamanazar47@gmail.com>
2024-06-27 21:05:19 +0300

    array of promise statuses

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,9 @@ import uploadPhoto from "./5-photo-reject"
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((res) => (
-	  res.map((result) => ({
+      res.map((result) => ({
         status: result.status,
-		value: result.status === 'fulfilled' ? result.value : String(result.reason),
-	  }))
-	));
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }))
+    ));
 }
