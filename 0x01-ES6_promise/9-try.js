735e7557077ab4de5a76ede65f46051c046e90ac
osamanazar47 <osamanazar47@gmail.com>
2024-06-27 22:39:03 +0300

    try and catch fixing style

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,13 @@
 export default function guardrail(mathFunction) {
 	const queue = [];
   
-	try {
+    try {
 	  queue.push(mathFunction());
-	} catch (err) {
-	  queue.push(String(err));
-	} finally {
-	  queue.push('Guardrail was processed');
-	}
-  
-	return queue;
+    } catch (err) {
+      queue.push(String(err));
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+
+    return queue;
   }
-  
