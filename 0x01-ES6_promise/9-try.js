5bcc297756f31c048c67adf444e8890a1ffaac2d
osamanazar47 <osamanazar47@gmail.com>
2024-06-27 22:31:40 +0300

    try and catch

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  let queue = [];
+  try {
+	queue.push(mathFunction())
+  } catch (error) {
+	queue.push(String(error))
+  }
+  finally {
+	queue.push('Guardrail was processed')
+  };
+  return queue
+}
