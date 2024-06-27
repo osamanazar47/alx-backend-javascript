663b2da3b1ae7b9978774366aabf5ff7bf76a0b2
osamanazar47 <osamanazar47@gmail.com>
2024-06-27 22:42:47 +0300

    advance task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let res = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
+}
