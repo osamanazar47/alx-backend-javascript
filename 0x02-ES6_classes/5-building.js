dbb640c35f0eac4ab4c673bf0ca5c135f55844ed
osamanazar47 <osamanazar47@gmail.com>
2024-06-28 02:13:37 +0300

    building class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
