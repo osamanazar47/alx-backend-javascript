e07c23945c61e1a667ecf2f39b107c6417ca7923
osamanazar47 <osamanazar47@gmail.com>
2024-07-03 02:24:55 +0300

    map the groceries list

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
