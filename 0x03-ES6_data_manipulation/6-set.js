87c05bc27ab8de3a1c13862fa1884a3c5ae3de36
osamanazar47 <osamanazar47@gmail.com>
2024-07-03 01:59:39 +0300

    checking every string in the set

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,4 @@
 export default function setFromArray(array) {
   return new Set(array);
+	
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  const resultArray = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  set.forEach((element) => {
+    if (typeof element === 'string' && element.startsWith(startString)) {
+      const substr = element.substring(startString.length);
+      resultArray.push(substr);
+    }
+  });
+  return resultArray.join('-');
+}
