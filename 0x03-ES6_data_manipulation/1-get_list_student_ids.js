8136d3d606699f229f2b7a3292e66f80caf53b42
osamanazar47 <osamanazar47@gmail.com>
2024-07-02 13:28:33 +0300

    student ids fixing style

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,8 @@
-let arr = [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-];
 export default function getListStudentIds(arr) {
   if (!Array.isArray(arr)) {
     return []; // Return an empty array if arr is not an array
   }
 
-  const arrIds = arr.map(student => student.id);
+  const arrIds = arr.map((student) => student.id);
   return arrIds;
 }
