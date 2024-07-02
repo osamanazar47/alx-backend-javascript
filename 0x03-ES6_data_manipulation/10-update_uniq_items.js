392fc6a41de616ea876ce11eb7dbaa0f2c44cc0c
osamanazar47 <osamanazar47@gmail.com>
2024-07-03 02:26:19 +0300

    all the rest tasks

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
