2b5f2cee13e1c82b6262e7b8a9763e4ed2a0d4ec
osamanazar47 <osamanazar47@gmail.com>
2024-07-02 23:53:16 +0300

    get sum of students ids using reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    const idsSum = students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+    return idsSum;
+  }
+  return [];
+}
