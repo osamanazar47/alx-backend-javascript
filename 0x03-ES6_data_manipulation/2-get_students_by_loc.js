1b4ede4b266a8a9324054b0952db6038d1125d20
osamanazar47 <osamanazar47@gmail.com>
2024-07-02 23:35:29 +0300

    get student by location using filter method

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,8 @@
 export default function getListStudentsByLocation(array, city) {
+  if (!Array.isArray(array)) {
+    return []; // Return an empty array if arr is not an array
+  }
+
   const result = array.filter((student) => student.location === city);
   return result;
 }
