f589a6e9cdbbb0547b5f9670a61b64ecb1ae7dcd
osamanazar47 <osamanazar47@gmail.com>
2024-07-02 13:07:01 +0300

    fixed a typo in the first task

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,3 @@
 export default function getListStudents() {
-  return [[1, 'Guill', 'San Francisco'], [2, 'James', 'Columbia'], [5, 'Serena', 'San Francisco']].map(([id, firstName, location]) => ({id, firstName, location}));
+  return [[1, 'Guillaume', 'San Francisco'], [2, 'James', 'Columbia'], [5, 'Serena', 'San Francisco']].map(([id, firstName, location]) => ({ id, firstName, location }));
 }
