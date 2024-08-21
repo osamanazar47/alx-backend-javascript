dc7db311cb79d9202c29c80f3f0766ee6f8b28ba
osamanazar47 <osamanazar47@gmail.com>
2024-08-21 22:52:23 +0300

    created a simple http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+})
+
+app.listen(1245, '127.0.0.1');
+
+module.exports = app
