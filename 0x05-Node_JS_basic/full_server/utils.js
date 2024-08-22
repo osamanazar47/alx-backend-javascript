91b9dafe9d3ce1d515f643655c65a4ad84868c06
osamanazar47 <osamanazar47@gmail.com>
2024-08-22 14:05:06 +0300

    complex express server fix

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,30 +1,28 @@
-const countStudents = require('../3-read_file_async');
-
 const fs = require('fs').promises;
 
-const readDatabase = async(path) => {
+const readDatabase = async (path) => {
   try {
     const data = await fs.readFile(path, 'utf-8');
     const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
 
-	if (lines.length <= 1){
-      return { message: 'No students found'};
-	}
-	const result = {
+    if (lines.length <= 1) {
+      return { message: 'No students found' };
+    }
+    const result = {
       CS: [],
-	  SWE: [],
-	};
-	for (let i = 1; i < lines.length; i += 1) {
+      SWE: [],
+    };
+    for (let i = 1; i < lines.length; i += 1) {
       const fields = lines[i].split(',');
-      const first_name = fields[0];
-	  const field = fields[3];
+      const firstName = fields[0];
+      const field = fields[3];
 
-	  if (field === 'CS') {
-        result.CS.push(first_name);
-	  } else if (field === 'SWE') {
-        result.SWE.push(first_name);
-	  }
-	}
+      if (field === 'CS') {
+        result.CS.push(firstName);
+      } else if (field === 'SWE') {
+        result.SWE.push(firstName);
+      }
+    }
     return result;
   } catch (err) {
     console.error(`Error reading file: ${err.message}`);
